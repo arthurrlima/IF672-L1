de8ad2f2643f9b3ad9866d1e860de9af85fd21c3
Arthur Lima <arl3@cin.ufpe.br>
2024-02-15 11:35:05 -0300

    Create ListaExtra_grafos.py

---
+++ ListaExtra_grafos.py
@@ -0,0 +1,19 @@
+class Grafo:
+    def __init__(self) -> None:
+        pass
+
+class Vertice:
+    def __init__(self) -> None:
+        pass
+
+class Aresta:
+    def __init__(self) -> None:
+        pass
+
+
+
+class Heap:
+    def __init__(self) -> None:
+        pass
+
+
